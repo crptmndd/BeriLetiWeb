51e52739a030ed96f34d3c55afeb8ddbc5305fe4
crptmndd <vadim.valeev.dev@gmail.com>
2025-04-24 12:46:10 +0300

    Added Secret Key from .env

--- app/app.py
+++ app/app.py
@@ -4,13 +4,13 @@ from app.routes.auth import router as auth_router
 from app.routes.main import router as main_router
 from app.api.v1.user import router as user_api_router
 from starlette.middleware.sessions import SessionMiddleware
-
+from app.config import SECRET_KEY
 
 app = FastAPI()
 
 # Подключение статических файлов
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
-app.add_middleware(SessionMiddleware, secret_key="DONOTUSE")
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 
 # Подключение маршрутов
 app.include_router(auth_router)
